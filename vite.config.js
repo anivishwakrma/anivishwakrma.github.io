d32a1ffdb68bde1e3c0b33ffb37778fd6b9a42f4
Aniket Vishwakarma <aniketvishwakarma1815@gmail.com>
2025-04-09 08:22:08 +0530

    Fix vite config base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Aniket-portfolio/',
+  base: '/Aniket-portfolio/', // 👈 This is important
 })
